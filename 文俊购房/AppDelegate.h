f35d10471f7ac96c5d42e66204b6b1a70f8a56d8
mac <mac@home.local>
2016-05-14 14:42:42 +0800

    Initial Commit

---
+++ 文俊购房/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  文俊购房
+//
+//  Created by mac on 16/5/14.
+//  Copyright © 2016年 wj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 文俊购房/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  文俊购房
+//
+//  Created by mac on 16/5/14.
+//  Copyright © 2016年 wj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
